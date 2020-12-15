2ab270c90f310d13691343a2754926e917ecbd0b
ikanago <28985004+ikanago@users.noreply.github.com>
2020-12-15 20:37:43 +0900

    feat: Define `Command` to represent function call to tables

--- src/lib.rs
+++ src/lib.rs
@@ -7,3 +7,9 @@ mod sstable;
 
 pub use crate::horreum::Horreum;
 pub use http::serve;
+
+pub(crate) enum Command<'a> {
+    Get { key: &'a [u8] },
+    Put { key: Vec<u8>, value: Vec<u8> },
+    Delete { key: &'a [u8] },
+}
